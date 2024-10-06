fc93cd1b99c427cef8c6f59dd4f5701e15ed9a76
alan-hynes <G00400498@atu.ie>
2024-10-06 23:15:13 +0100

    populated order entity and added new dependency

--- src/main/java/ie/atu/orderservice/Order.java
+++ src/main/java/ie/atu/orderservice/Order.java
@@ -1,4 +1,24 @@
 package ie.atu.orderservice;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+
+    @Digits(integer = 5, fraction = 0, message = "Order ID must be 5 digits")
+    private Long id;
+
+    @Email(message = "Invalid email")
+    private String email;
+
+    @Min(value = 0, message = "Total price cannot be negative")
+    private double totalPrice;
+
 }
